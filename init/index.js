c580dd6b3e671bffc4509b97529239bfc10a06ca
nitin samson <nitinsamson095@gmail.com>
2024-05-28 10:24:11 +0530

    msg

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) =>({...obj, owner:"6652cd0bf42f1decf21412f2"}));
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
